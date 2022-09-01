dbb345aeee35bf1aea36aebf9e315e7456d46f60
07arif <ai.arif0711@gmail.com>
2022-09-01 14:30:39 +0600

    summary

---
+++ js/summary.js
@@ -0,0 +1,29 @@
+
+/*
+1. Fetch must porvide url{dynamic or static}
+2. how to convert fetch promise return to Json
+3.how to convert json to data
+4.[cool headed]:
+users:
+--> array of objects
+--> object with property users
+--> object with a property called data
+
+nested object
+
+------------------------
+DOM manipulation
+
+1.get somthing from the DOm
+2.create element to append to the dom
+3.dynamically load data on id
+-------------------------
+array --forEach, map, filter
+----------------
+template string 
+---------------
+
+
+
+
+*/ 
