8d7eb0f8405d7593964809858470f19684869b4f
07arif <ai.arif0711@gmail.com>
2022-09-01 08:05:58 +0600

    Implement Show All button to display all data

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
-    //display 100 phones only
+    //display 10 phones only
     const showAll = document.getElementById('show-all');
-    if (phones.length > 10) {
+    if (dataLimit && phones.length > 10) {
         phones = phones.slice(0, 10);
         showAll.classList.remove('d-none')
     }
@@ -47,14 +47,18 @@ const displayPhones = phones => {
     toggleSpinner(false);
 }
 
+const processSearch = (dataLimit) => {
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+
+}
 
 // handle search button click
 document.getElementById('btn-search').addEventListener('click', function () {
     // start loader
-    toggleSpinner(true);
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    loadPhones(searchText);
+    processSearch(10);
 
 })
 
@@ -67,4 +71,13 @@ const toggleSpinner = isLoading => {
         loaderSection.classList.add('d-none');
     }
 }
+
+
+
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click', function () {
+    processSearch();
+
+
+})
 // loadPhones()
